d45441d0cd018c629bfac7b92d073a2a6337c9ef
Steven <xu.steven.wc@gmail.com>
2025-02-11 16:12:42 +0800

    say Drake!

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
+
+
+</script>
+
+
+<template>
+  <Navbar />
+  <Hero />
+  <HomeCards />
+  <JobListings />
+</template>
+
---
+++ src/App2.vue
@@ -0,0 +1,50 @@
+<script>
+export default{
+    // options API() and interpolation
+    data(){
+      return{
+        name: 'John Doe',
+        status: 'pending', 
+        tasks: ['task one', 'task two', 'task three'],
+        link: 'https://google.com'
+      };
+    },
+    methods: {
+      toggleStatus(){
+        if (this.status ==='active'){
+          this.status ='pending';
+        } else if (this.status === 'pending'){
+          this.status ='inactive';
+        } else {
+          this.status = 'active';
+        }
+      },
+    },
+  };
+
+</script>
+
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+
+  </ul>
+  <!-- <a v-bind:href="link">Click for google</a> -->
+  <a :href="link">Click for google</a>
+  <br />
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
+</template>
+
+<style scoped>
+  h1 {
+    color: red;
+  }
+</style>
