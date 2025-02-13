122beaa69f5d2221578167333e571c4bb210e8ef
wenchenxu <xu.steven.wc@gmail.com>
2025-02-13 11:19:44 +0800

    add 'more' and 'less' buttons in job descriptions.

--- src/components/JobListing.vue
+++ src/components/JobListing.vue
@@ -1,9 +1,23 @@
 <script setup>
-import { defineProps } from 'vue';
+import { defineProps, ref, computed } from 'vue';
 
-defineProps({
+const props = defineProps({
   job: Object
 });
+
+const showFullDescription = ref(false);
+
+const toggleFullDescription = () =>{
+  showFullDescription.value = !showFullDescription.value;
+}
+
+const truncatedDescription = computed(() => {
+  let description = props.job.description;
+  if (!showFullDescription.value){
+    description = description.substring(0,90) + '...';
+  }
+  return description;
+})
 </script>
 
 <template>
@@ -15,7 +29,12 @@ defineProps({
               </div>
 
               <div class="mb-5">
-                {{ job.description }}
+                <div>
+                  {{ truncatedDescription}}
+                </div>
+                <button @click="toggleFullDescription" class="text-green-500 hover:text-green-600 mb-5">
+                  {{ showFullDescription ? 'Less' : 'More' }}
+                </button>
               </div>
 
               <h3 class="text-green-500 mb-2">{{ job.salary }}</h3>
